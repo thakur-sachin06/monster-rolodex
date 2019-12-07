4d6270719909aa4ca468940c24a0e301505c8db8
Sachin Thakur <sachinst000@mail.com>
2019-12-07 15:07:14 +0530

    Updates

---
+++ precache-manifest.054fa3100d321f45221938bb87323848.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e5ab097b17ff2f7db813d957584773",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0b5c90d99105bab2804b",
+    "url": "/monster-rolodex/static/css/main.ebeb65d2.chunk.css"
+  },
+  {
+    "revision": "2ea260805c2d8eeb767a",
+    "url": "/monster-rolodex/static/js/2.bce2a7e5.chunk.js"
+  },
+  {
+    "revision": "0b5c90d99105bab2804b",
+    "url": "/monster-rolodex/static/js/main.0488849c.chunk.js"
+  },
+  {
+    "revision": "aff08a4d83f58743dcb3",
+    "url": "/monster-rolodex/static/js/runtime-main.400ce9bc.js"
+  }
+]);
